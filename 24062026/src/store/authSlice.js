9e60e7caf258e92ce7bb872ab800bf817d4d5511
Esra Sağın <152014236+esrasaginn@users.noreply.github.com>
2026-06-24 15:55:09 +0300

    24062026

---
+++ 24062026/src/store/authSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isAuth: false,
+  activeTab: "login",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuth = true;
+      state.user = {
+        name: "Esra S.",
+        role: action.payload,
+      };
+      state.activeTab = "dashboard";
+    },
+    logout: (state) => {
+      state.isAuth = false;
+      state.user = null;
+      state.activeTab = "login";
+    },
+
+    setActiveTab: (state, action) => {
+      state.activeTab = action.payload;
+    },
+  },
+});
+
+export const { login, logout, setActiveTab } = authSlice.actions;
+export default authSlice.reducer;
